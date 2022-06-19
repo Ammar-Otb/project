c84488e8d49b6d57283cf9ba23da49b15e6a2a87
Ammar Otaibi <76530937+Ammar-to@users.noreply.github.com>
2022-06-19 13:47:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-
-function getCity(){
-    let city = document.getElementById("city").value;
-    return city;
+async function getCity() {
+  var cityName = document.getElementById("city").value;
+  console.log(cityName);
+  await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=af7ddd3322a14a1c588d8a42229c806b`
+    ).then((resp)=> resp.json())
+    .then((data)=> {
+      document.getElementById("humidity").innerText = data["main"].humidity;
+      document.getElementById("temp").innerText = data["main"].temp;
+      document.getElementById("desc").innerText = data.weather[0].description;
+    });
 }
-
-async function weather(){
-    let data = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${getCity()}&units=metric&appid=af7ddd3322a14a1c588d8a42229c806b`
-    );
-    let info= await data.json();
-    return info
-}
-
-
-
-document.querySelector("#searchBtn").addEventListener('click', ()=> {
-        document.getElementById("humidity").innerText = weather.main.humidity;
-        document.getElementById("temp").innerText = weather.main.temp;
-        document.getElementById("humidity").innerText = weather.weather.description;
-});
-
-
-
